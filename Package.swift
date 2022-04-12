265768cc4c9602999a7291fcf23196760e057112
Jeremy Blaker <jblaker@brightcove.com>
2022-04-12 12:41:45 -0400

    blah

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 
 import PackageDescription
 
-let BrightcovePlayerSDK = Package(
-    name: "SSAI Plugin for Brightcove Player SDK",
-    platforms: [
-        .iOS(.v11), .tvOS(.v11)
-    ],
-    products: [
-        .library(
-            name: "BrightcoveSSAI",
-            targets: ["BrightcoveSSAI", "BrightcovePlayerSDK"])
-    ],
-    dependencies: [    
-        .package(url: "https://github.com/jblaker/spm-core.git", from: "6.10.4")
-    ],
-    targets: [
-        .binaryTarget(
-            name: "BrightcoveSSAI",
-            path: "xcframework/BrightcoveSSAI.xcframework"
-        )
-    ]
-)
+// let BrightcovePlayerSDK = Package(
+//     name: "SSAI Plugin for Brightcove Player SDK",
+//     platforms: [
+//         .iOS(.v11), .tvOS(.v11)
+//     ],
+//     products: [
+//         .library(
+//             name: "BrightcoveSSAI",
+//             targets: ["BrightcoveSSAI", "BrightcovePlayerSDK"])
+//     ],
+//     dependencies: [    
+//         .package(url: "https://github.com/jblaker/spm-core.git", from: "6.10.4")
+//     ],
+//     targets: [
+//         .binaryTarget(
+//             name: "BrightcoveSSAI",
+//             path: "xcframework/BrightcoveSSAI.xcframework"
+//         )
+//     ]
+// )
