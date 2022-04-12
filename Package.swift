57cbbf0c88a0fc1dee9fc27ce75e1e34ca273daa
Jeremy Blaker <jblaker@brightcove.com>
2022-04-12 11:54:32 -0400

    blah

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+// https://developer.apple.com/documentation/xcode/creating_a_standalone_swift_package_with_xcode?language=objc
+
+import PackageDescription
+
+let BrightcovePlayerSDK = Package(
+    name: "SSAI Plugin for Brightcove Player SDK",
+    platforms: [
+        .iOS(.v11), .tvOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "BrightcoveSSAI",
+            targets: ["BrightcoveSSAI"])
+    ],
+    dependencies: [    
+        .package(url: "https://github.com/jblaker/spm-core.git", from: "1.0.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BrightcoveSSAI",
+            path: "xcframework/BrightcoveSSAI.xcframework",
+            dependencies: ["BrightcovePlayerSDK"]
+        )
+    ]
+)
